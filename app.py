6077f5584ec3d1f61985b0ed52648265dcc0ccc6
Pawel Weber <pawelweber@yahoo.pl>
2016-03-24 21:30:09 +0100

    Add app content

---
+++ app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class PzwlaFileExplorerConfig(AppConfig):
+    name = "pzwla_filer_explorer"
+    verbose_name = "Modul pliki"
