e9e3616d5de89c28adbec6aa1fd4d1ae4c2d2251
Pawel Weber <pawelweber@yahoo.pl>
2016-03-24 22:00:42 +0100

    change app name

--- cms_apps.py
+++ cms_apps.py
@@ -9,6 +9,6 @@ from django.utils.translation import ugettext_lazy as _
 class PzwlaFileExplorerApp(CMSApp):
     name = _("Przegladarka plikow")  # give your app a name, this is required
     urls = ["pzwla_file_explorer.urls"]  # link your app to url configuration(s)
-    app_name = "pliki"
+    app_name = "pzwla_file_explorer"
 
 apphook_pool.register(PzwlaFileExplorerApp)  # register your app
