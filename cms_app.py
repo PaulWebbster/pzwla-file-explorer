fe18319b894573687afc228c23ec6f01518edeb6
Pawel Weber <pawelweber@yahoo.pl>
2016-03-24 21:26:15 +0100

    Add cms app

---
+++ cms_app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cms.app_base import CMSApp
+from cms.apphook_pool import apphook_pool
+from django.utils.translation import ugettext_lazy as _
+
+
+class PzwlaFileExplorerApp(CMSApp):
+    name = _("Przegladarka plikow")  # give your app a name, this is required
+    urls = ["pzwla_file_explorer.urls"]  # link your app to url configuration(s)
+    app_name = "pliki"
+
+apphook_pool.register(PzwlaFileExplorerApp)  # register your app
