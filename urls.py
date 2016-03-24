7f28af809a81c174a255134acd18da6f596f47f6
Pawel Weber <pawelweber@yahoo.pl>
2016-03-24 21:19:54 +0100

    Change urls

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from views import RootFolderView
 from views import SubFolderView
 urlpatterns = [
-    url(r'^pliki/$', RootFolderView.as_view()),
+    url(r'^$', RootFolderView.as_view()),
     url(r'^subfolder/(?P<pk>[\w]+)/$', SubFolderView.as_view()),
 ]
